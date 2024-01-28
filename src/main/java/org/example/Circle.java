73f337574d77715d60c3caea5baadc628c97a164
Pyanuk <144242312+Pyanuk@users.noreply.github.com>
2024-01-29 00:49:17 +0300

    pr1

--- src/main/java/org/example/Circle.java
+++ src/main/java/org/example/Circle.java
@@ -4,13 +4,21 @@ import java.util.Scanner;
 public class Circle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите радиус круга: ");
+        System.out.println("Введите радиус: ");
         double radius = scanner.nextDouble();
+
+        System.out.println("Введите периметр: ");
+        double perimetr = scanner.nextDouble();
+
+        System.out.println("Введите площадь: ");
+        double ploschad = scanner.nextDouble();
 
         double area = Math.PI * Math.pow(radius, 2);
         double circumference = 2 * Math.PI * radius;
+        double radiys = ploschad / 2 * Math.PI;
 
-        System.out.println("Площадь круга: " + area);
-        System.out.println("Периметр круга: " + circumference);
+        System.out.println("Площадь: " + area);
+        System.out.println("Периметр: " + circumference);
+        System.out.println("Радиус: " + radiys);
     }
 }
